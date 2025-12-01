ad5f654a6145113288189d7e98bf4914067c37e8
Brentador <brent.devroey@gmail.com>
2025-12-01 09:50:01 +0100

    Initial Commit

---
+++ FocusPond/FocusPondApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusPondApp.swift
+//  FocusPond
+//
+//  Created by studentehb on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusPondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
